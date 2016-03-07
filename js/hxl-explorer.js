6779f9ac3aa8b2b7c7b03d41783098125c4a6eeb
David Megginson <david.megginson@megginson.com>
2016-03-07 17:00:05 -0500

    Modularise code a bit more, including nextCard and previousCard functions.

--- js/hxl-explorer.js
+++ js/hxl-explorer.js
@@ -21,16 +21,46 @@ hxlExplorer.views.cards = function (hxlData) {
     var isFirst = true;
     var slider;
 
+    function gotoCard(index) {
+        $('#hxl-cards').carousel(index);
+    }
+
+    function previousCard() {
+        gotoCard(Math.max(getActiveIndex()-1, 0))
+    }
+
+    function nextCard() {
+        gotoCard(Math.min(getActiveIndex()+1, hxlData.rows.length-1))
+    }
+
     function setupSlider () {
         slider = new Slider('#slider', {
             min: 1,
-            max: hxlData.rows.length - 1,
+            max: hxlData.rows.length,
             value: 1
         });
 
         // Update the carousel
         $('#slider').on('slideStop', function () {
-            $('#hxl-cards').carousel(slider.getValue()-1);
+            gotoCard(slider.getValue()-1);
+        });
+
+        $('.slider-container').removeClass('hidden');
+
+        $('#card-start').click(function () {
+            gotoCard(0);
+        });
+
+        $('#card-prev').click(function () {
+            previousCard();
+        });
+
+        $('#card-next').click(function () {
+            nextCard();
+        });
+
+        $('#card-end').click(function () {
+            gotoCard(hxlData.rows.length-1);
         });
     }
 
@@ -39,16 +69,16 @@ hxlExplorer.views.cards = function (hxlData) {
     }
 
     function updateHeader() {
-        var index = $(innerNode).find('.active').index();
-        $(sectionNode).find('h2').text('Record ' + (index + 1) + " of " + hxlData.rows.length);
+        $('#data-title').text('Record ' + slider.getValue() + " of " + hxlData.rows.length);
     }
 
     function formatCard(row) {
         var cardNode = $('<table class="table hxl-card item">');
         for (var i = 0; i < hxlData.columns.length && i < row.values.length; i++) {
             var tableRowNode = $('<tr>');
-            tableRowNode.append($('<th>').text(hxlData.columns[i].header || hxlData.columns[i].displayTag));
-            tableRowNode.append($('<td>').text(row.values[i]));
+            tableRowNode.append($('<th class="header">').text(hxlData.columns[i].header));
+            tableRowNode.append($('<th class="hashtag">').text(hxlData.columns[i].displayTag));
+            tableRowNode.append($('<td class="value">').text(row.values[i]));
             cardNode.append(tableRowNode);
         }
         return cardNode;
@@ -66,21 +96,20 @@ hxlExplorer.views.cards = function (hxlData) {
 
     // example for detecting slide changes
     $(cardsNode).on('slid.bs.carousel', function () {
-        updateHeader();
         slider.setValue(getActiveIndex() + 1);
+        updateHeader();
     });
 
     $(cardsNode).on('swipeleft', function (e) {
-        $(this).carousel('next');
+        nextCard();
     });
 
     $(cardsNode).on('swiperight', function (e) {
-        $(this).carousel('prev');
+        previousCard();
     });
 
     setupSlider();
     
-    sectionNode.append($('<h2>'));
     sectionNode.append(cardsNode);
     updateHeader();
     return sectionNode;
